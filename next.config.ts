8f7d4d09aa982877ccf142d89c6ffafcd3fe6ce2
Hasanain <hasanaintushar@gmail.com>
2026-04-18 14:55:32 +0600

    update configuration files

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  distDir: process.env.NETLIFY ? ".netlify/output" : ".next",
   turbopack: {
     root: path.resolve(__dirname),
   },
